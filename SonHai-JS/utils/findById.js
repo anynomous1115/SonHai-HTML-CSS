5045b51c10785a3d34c79f8eb0776874f1a194c6
unknown <caosonhai1410@gmail.com>
2023-09-12 11:32:49 +0700

    REST API

--- SonHai-JS/utils/findById.js
+++
@@ -1,9 +0,0 @@
-import { products } from "../ui-global-state/state.js"
-
-const getProductById = ( key) => {
-   const product= products.find(data => data.id == key.id)
-   return product
-}
-export {
-    getProductById
-}
